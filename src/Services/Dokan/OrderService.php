3b3b893ad139e57f82c8585c187049696f242938
Yunus ALTIN <yunusaltin1963@gmail.com>
2022-07-25 09:58:54 +0300

    Dokan duzeltildi

--- src/Services/Dokan/OrderService.php
+++ src/Services/Dokan/OrderService.php
@@ -35,10 +35,9 @@ class OrderService extends BaseService
      *      (int) => shipmentPackageIds,
      *      (int) page,
      *      (int) size => (max) 200,
-     *      (string) orderByField => PackageLastModifiedDate, CreatedDate,
      *      (string) orderByDirection => DESC, ASC,
-     *      (int) startDate => Tarih aralığı max 2 hafta,
-     *      (int) endDate => Tarih aralığı max 2 hafta
+     *      (int) startDate,
+     *      (int) endDate
      * ]
      * @return object $response
      */
@@ -50,9 +49,11 @@ class OrderService extends BaseService
         }
 
         $query = [
+            'status'        => $data['status'],
             'sku'           => $data['barcode'],
             'page'          => $data['page'] ? $data['page'] + 1 : 1,
             'per_page'      => $data['size'] ?? 50,
+            'order'         => $data['orderByDirection'] ?? 50,
         ];
 
         if ($data['startDate']) {
@@ -63,40 +64,40 @@ class OrderService extends BaseService
             $query["before"] = $data['endDate'];
         }
 
-        $response = $this->client->get("https://stageapi.trendyol.com/stagesapigw/suppliers/{$this->merchantId}/orders", $query);
+        $response = $this->client->get($this->httpUrl . "/wp-json/dokan/v1/orders", $query);
 
         return $response;
     }
 
 
+    // /**
+    //  * @param int $id
+    //  * @return object
+    //  */
+    // public function updateProduct($id, $data)
+    // {
+    //     $result = $this->client->put($this->httpUrl . "/wp-json/dokan/v1/products/{$id}", $data);
+
+    //     return $result;
+    // }
+
+    // /**
+    //  * @param int $id
+    //  * @return object
+    //  */
+    // public function deleteProduct($id)
+    // {
+    //     $result = $this->client->delete($this->httpUrl . "/wp-json/dokan/v1/products/{$id}");
+
+    //     return $result;
+    // }
+
     /**
-     * @param int $id
      * @return object
      */
-    public function updateProduct($id, $data)
+    public function getOrdersSummary()
     {
-        $result = $this->client->put($this->httpUrl . "/wp-json/dokan/v1/products/{$id}", $data);
-
-        return $result;
-    }
-
-    /**
-     * @param int $id
-     * @return object
-     */
-    public function deleteProduct($id)
-    {
-        $result = $this->client->delete($this->httpUrl . "/wp-json/dokan/v1/products/{$id}");
-
-        return $result;
-    }
-
-    /**
-     * @return object
-     */
-    public function getProductsSummary()
-    {
-        $result = $this->client->get($this->httpUrl . "/wp-json/dokan/v1/products/summary");
+        $result = $this->client->get($this->httpUrl . "/wp-json/dokan/v1/orders/summary");
 
         return $result;
     }
